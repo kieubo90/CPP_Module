343c69d8ab2cb943db8fc17328c817b2c842c90c
Vu Kieu Phong Vu <vuvu.connect@gmail.com>
2023-12-19 15:17:38 +0200

    Add files via upload

---
+++ Module_02/ex02/main.cpp
@@ -0,0 +1,20 @@
+#include "Fixed.hpp"
+
+int main( void ) {
+Fixed a;
+Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
+std::cout << a << std::endl;
+std::cout << ++a << std::endl;
+std::cout << a << std::endl;
+std::cout << a++ << std::endl;
+std::cout << a << std::endl;
+std::cout << b << std::endl;
+std::cout << Fixed::max( a, b ) << std::endl;
+
+
+Fixed c (10.2f);
+Fixed d (21.4f);
+Fixed e = d - c;
+std::cout<< e <<std::endl;
+return 0;
+}
