a210e04cd7070bd76be2a29b9fd448e99a013703
Vu Kieu Phong Vu <vuvu.connect@gmail.com>
2023-12-27 22:39:01 +0200

    Add files via upload

---
+++ Module_04/ex00/src/main.cpp
@@ -0,0 +1,23 @@
+#include "../include/Animal.hpp"
+#include "../include/Cat.hpp"
+#include "../include/Dog.hpp"
+#include "../include/WrongAnimal.hpp"
+#include "../include/WrongCat.hpp"
+
+int main(){
+
+    const Animal* meta = new Animal();
+    const Animal* j = new Dog();
+    const Animal* i = new Cat();
+
+    std::cout << j->getType() << " " << std::endl;
+    std::cout << i->getType() << " " << std::endl;
+    i->makeSound(); //will output the cat sound!
+    j->makeSound();
+    meta->makeSound();
+
+    delete i;
+    delete j;
+    delete meta;
+    return (0);
+}
