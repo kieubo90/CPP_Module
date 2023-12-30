59875ecd1da1198b9af58be5af7ff214a2eda648
Vu Kieu Phong Vu <vuvu.connect@gmail.com>
2023-12-30 19:07:30 +0200

    Add files via upload

---
+++ Module_04/ex01/src/WrongAnimal.cpp
@@ -0,0 +1,30 @@
+#include "../include/WrongAnimal.hpp"
+
+WrongAnimal::WrongAnimal(): _type("WrongDefault"){
+    std::cout << "WrongAnimal Default Constructor called" << std::endl;
+}
+
+WrongAnimal::WrongAnimal(const WrongAnimal &other){
+    *this = other;
+    std:: cout << "WrongAninmal Copy Constructor called" << std::endl;
+}
+
+WrongAnimal::~WrongAnimal(){
+    std::cout << "WrongAnimal Destructor called" << std::endl;
+}
+
+WrongAnimal &WrongAnimal::operator=(const WrongAnimal &other){
+    if (this != &other){
+        std::cout << "WrongAnimal Assignment called" << std::endl;
+        this->_type = other._type;
+    }
+    return (*this);
+}
+
+void    WrongAnimal::makeSound(void) const{
+    std::cout << this->_type << " WrongAnimalllllllll" << std::endl;
+}
+
+std::string WrongAnimal::getType(void) const{
+    return (this->_type);
+}
